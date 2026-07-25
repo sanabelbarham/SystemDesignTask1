dcef0834f27bed5a4b04220b4a9858fdd293bcfd
Sanabel Barham <sanabelbarham123@gmail.com>
2026-07-25 12:36:59 +0300

    task done

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemDesignTask1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class TaskController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TaskController (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetRequest()
+        {
+            var servername = _configuration["ServerName"];
+            return Ok($"hello from {servername}");
+        }
+
+    }
+}
